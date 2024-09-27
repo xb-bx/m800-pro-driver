19daa9b0f02a4ca710ae41892cbea83799222caa
xb-bx <nazarnik050406@gmail.com>
2024-09-27 15:36:00 +0300

    set current polling rate

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Linux driver for Delux M800 PRO mouse
 # Capabilities
     - [X] Get battery charge
-    - [ ] Get/set current polling rate
+    - [-] Get/set current polling rate
     - [ ] Remap keys
 # Requirements
     - C compiler
--- main.c
+++ main.c
@@ -1,11 +1,105 @@
 #include <libusb-1.0/libusb.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #define M800_PRO_VID 0x248a
 #define M800_PRO_PID 0x5b2f
 #define INTERFACE 1
 #define eprintf(args...) fprintf(stderr, args)
-int main() {
+
+enum PollingRate {
+    Hz125   = 0x8,
+    Hz250   = 0x4,
+    Hz500   = 0x2,
+    Hz1000  = 0x1,
+};
+
+void
+usage() {
+    printf("m800pro-drv <command> <args>\n");
+    printf("commands:\n");
+    printf("\tquery (charge|poll)\n");
+    printf("\tset    poll (125|250|500|1000)\n");
+}
+int
+query(int argc, char **argv, libusb_device_handle *dev_handle, int charge, int packet_id) {
+    if(argc == 0) {
+        usage(); 
+        return -1;
+    }
+    if(strcmp("charge", argv[0]) == 0) {
+        printf("%i\n", charge);
+        return 0;
+    } else if(strcmp("poll", argv[0]) == 0) {
+        eprintf("NOT IMPLEMENTED\n");
+        return -1;
+    } else {
+        eprintf("Unknown\n");
+        return -1;
+    }
+}
+int
+set_polling_rate(libusb_device_handle *dev_handle, int packet_id, int polling_rate) {
+    unsigned char payload[33] = {0xc, 0x1, 0x7, 0x0, 0x32, 0x1, 0x1, 0x8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    payload[4] = packet_id + 1;
+    payload[7] = polling_rate;
+    int n = libusb_control_transfer(dev_handle, 0x21, 0x9, 0x30c, 1, payload, 33, 0);
+    return n;
+}
+int
+set(int argc, char **argv, libusb_device_handle *dev_handle, int packet_id) {
+    if(argc < 2) {
+        usage(); 
+        return -1;
+    }
+    if(strcmp("poll", argv[0]) == 0) {
+        int polling_rate = 0;
+        if(strcmp("125", argv[1]) == 0) {
+            polling_rate = Hz125;
+        } else if(strcmp("250", argv[1]) == 0) {
+            polling_rate = Hz250;
+        } else if(strcmp("500", argv[1]) == 0) {
+            polling_rate = Hz500;
+        } else if(strcmp("1000", argv[1]) == 0) {
+            polling_rate = Hz1000;
+        } else {
+            eprintf("Unsupported polling rate\n");
+            return -1;
+        }
+        return set_polling_rate(dev_handle, packet_id, polling_rate);
+    } else {
+        eprintf("Unknown\n");
+        return -1;
+    }
+}
+
+int 
+initial_query(libusb_device_handle *dev_handle, int *packet_id) {
+    int i = 1;
+    while(1) {
+        if(i == 0) {
+            return 0;
+        }
+        unsigned char data[] = {0xc, 0x1, 0x20, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+        data[4] = i++;
+        int n = libusb_control_transfer(dev_handle, 0x21, 0x9, 0x30c, 1, data, 33, 0);
+        unsigned char buf[33] = {0};
+        n = libusb_control_transfer(dev_handle, 0xa1, 0x1, 0x30c, 1, buf, 33, 0);
+        if(n < 0) {
+            return n;
+        }
+        if(buf[18]) {
+            *packet_id = i - 1;
+            return buf[18];
+        }
+    }
+}
+int 
+main(int argc, char **argv) {
+    if(argc < 2) {
+        usage();
+        return 0;
+    }
     libusb_context *ctx = NULL;
     int ret = libusb_init(&ctx);
     if (ret != LIBUSB_SUCCESS) {
@@ -45,22 +139,20 @@ int main() {
         eprintf("ERROR: %i %i %s\n", ret, __LINE__, libusb_error_name(ret));
         return 1;
     }
-    int res = 0;
-    int i = 1;
-    while(res == 0) {
-        unsigned char data[] = {0xc, 0x1, 0x20, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-        data[4] = i++;
-        int n = libusb_control_transfer(dev_handle, 0x21, 0x9, 0x30c, 1, data, 33, 0);
-        unsigned char buf[33] = {0};
-        n = libusb_control_transfer(dev_handle, 0xa1, 0x1, 0x30c, 1, buf, 33, 0);
-        if(n < 0) {
-            printf("ERROR: %i %i %s\n", n, __LINE__, libusb_error_name(n));
-            return 1;
-        }
-        if(buf[18])
-        printf("%i\n", buf[18]);
-        res = buf[18];
+
+    int id = 0;
+    int charge = initial_query(dev_handle, &id);
+    if(charge <= 0) {
+        eprintf("ERROR: FAILED TO GET BATTERY CHARGE %i %s\n", charge, libusb_error_name(charge));
+        return -1;
     }
+
+    if(strcmp("query", argv[1]) == 0) {
+        query(argc-2, argv+2, dev_handle, charge, id);
+    } else if(strcmp("set", argv[1]) == 0) {
+        set(argc-2, argv+2, dev_handle, id);
+    }
+
     libusb_release_interface(dev_handle, INTERFACE);
     if(has_driver) {
         ret = libusb_attach_kernel_driver(dev_handle, INTERFACE);
